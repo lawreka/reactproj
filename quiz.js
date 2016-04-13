80a69ad98c683b247f0d013a28a5542a5379e7f7
lawreka <lawr33ka@gmail.com>
2016-04-12 23:12:28 -0700

    made questions appear and button to get result work

--- quiz.js
+++ quiz.js
@@ -1,49 +1,76 @@
+var data = [
+  {id: 1, number: 1, text: "This is the question"},
+  {id: 2, number: 2, text: "This is another question"},
+  {id: 3, number: 3, text: "This is a third"},
+  {id: 4, number: 4, text: "This is the fourth"},
+  {id: 5, number: 5, text: "Fifth"},
+  {id: 6, number: 6, text: "Sixth"},
+  {id: 7, number: 7, text: "Etc etc"}
+];
+
 var Quiz = React.createClass({
   render: function() {
     return (
       <div>
-        <Question />
-        <Questext />
-        <h1>Result</h1>
+        <QuestionList data={this.props.data}/>
+        <Result />
       </div>
     )
   }
 });
-var questions = ['1', '2', '3'];
-var texts = ['uno', 'dos', 'tres'];
+var QuestionList = React.createClass({
+  render: function() {
+    var questions = this.props.data.map(function(question) {
+      return(
+        <Question number={question.number} key={question.id}>
+          {question.text}
+        </Question>
+      );
+    });
+    return (
+      <div className="questionList">
+        {questions}
+      </div>
+    )
+  }
+});
 var Question = React.createClass({
   render: function() {
     return (
-      <div>
-      {
-        questions.map(function (question){
-          return (
-            <div>Question #{question}</div>
-          )
-        })
-      }
+      <div className="questiontext">
+        <h2 className="questionid">
+          Question # {this.props.number}
+        </h2>
+        {this.props.children}
       </div>
-    )
+    );
   }
 });
-var Questext = React.createClass({
+
+var Result = React.createClass({
+  getInitialState: function () {
+    return {
+      opacity: 0
+    };
+  },
+  handleClick: function() {
+    this.setState({
+      opacity: 1.0
+    });
+  },
   render: function() {
     return (
       <div>
-      {
-        texts.map(function (tex){
-          return (
-            <div>{tex}</div>
-          )
-        })
-      }
+        <input type="button" value="Get Result" onClick={this.handleClick} />
+        <div style={{opacity:this.state.opacity}}>
+          This is a result.
+        </div>
       </div>
     )
   }
 });
 
-
 ReactDOM.render(
-  <Quiz />,
+  <Quiz data={data}/>,
   document.getElementById('content')
 );
